a05a6ce808ef063425988556f9f68f2ac7c5b18f
Angelo Vicente Filho <angelovicentefilho@gmail.com>
2021-09-22 20:04:37 -0300

    [main] setup

---
+++ cqrs-project/user-core/src/test/java/br/com/jtech/cloud/user/core/UserCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jtech.cloud.user.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ cqrs-project/user-query-api/src/test/java/br/com/jtech/cloud/user/query/api/UserQueryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jtech.cloud.user.query.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserQueryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
